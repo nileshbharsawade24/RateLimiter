ad45c91bed26cae23bd144f38a7e7fa430283d68
nileshbharsawade24 <nileshbharsawade24@gmail.com>
2024-09-15 19:57:44 +0530

    added log

--- src/main/java/com/example/RateLimiter/configs/RateLimiterConfig.java
+++ src/main/java/com/example/RateLimiter/configs/RateLimiterConfig.java
@@ -3,6 +3,7 @@ package com.example.RateLimiter.configs;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Bucket4j;
 import io.github.bucket4j.Bandwidth;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @Configuration
+@Slf4j
 public class RateLimiterConfig {
 
     private final ConcurrentMap<String, Bucket> bucketMap = new ConcurrentHashMap<>();
@@ -19,6 +21,7 @@ public class RateLimiterConfig {
     public void registerRateLimit(String methodName, int requestsPerMinute) {
         Bandwidth limit = Bandwidth.simple(requestsPerMinute, Duration.ofSeconds(60));  // Refills more frequently
         Bucket bucket = Bucket4j.builder().addLimit(limit).build();
+        log.info("ratelimiter added for method {} with requestsPerMinute {}", methodName, requestsPerMinute);
         bucketMap.put(methodName, bucket);
     }
 
